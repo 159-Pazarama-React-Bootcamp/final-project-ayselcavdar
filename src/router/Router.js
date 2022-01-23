84ff274937beb9ee16af4946cce4fc22b0b8d141
Aysel Cavdar <a.misrikhanova13@gmail.com>
2022-01-24 02:10:52 +0300

    create protected routes for applicants and admin

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,17 +1,30 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import React from 'react';
 import CreateApplication from '../pages/create-application';
 import SuccessfullApplication from '../pages/applications';
 import ApplicationDetail from '../pages/application-detail';
 import NotFound from '../components/not-found/NotFound';
+import ProtectedRoutes from './ProtectedRoutes';
+import AdminLogin from '../pages/admin/login';
+import ApplicationList from '../pages/admin/application-list';
+
 
 const AppRouter = () => {
+  const isAdmin = true;
   return (
     <Router>
       <Routes>
         <Route path="/" element={<CreateApplication />} />
-        <Route path="basvurular" element={<SuccessfullApplication />} />
-        <Route path="basvurular/:id" element={<ApplicationDetail />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path="basvurular" element={<SuccessfullApplication />} />
+          <Route path="basvurular/:basvuruNo" element={<ApplicationDetail />} />
+        </Route>
+        {isAdmin && (
+          <>
+            <Route path="admin" element={<AdminLogin />} />
+            <Route path="admin/basvuru-listesi" element={<ApplicationList />} />
+            <Route path="admin/basvuru-listesi/:basvuruNo" element={<p>details</p>} />
+          </>
+        )}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
