36fb2d77a994593cf71da5ff592397d23f6c10f9
Aysel Cavdar <a.misrikhanova13@gmail.com>
2022-01-25 15:48:17 +0300

    set user(admin) when it is already authenticated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import AppRouter from './router/Router';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { auth } from './utils/firebase.utils';
+import { setUser } from './redux/actions/loginActions';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+   onAuthStateChanged(auth, (user) => dispatch(setUser(user)));
+  }, [dispatch]);
+
   return (
     <main>
-     <AppRouter/>
+      <AppRouter />
     </main>
   );
 }
