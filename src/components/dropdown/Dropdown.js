2488a6b8db21b727871c40d812ce044003ced253
Aysel Cavdar <a.misrikhanova13@gmail.com>
2022-01-26 18:43:13 +0300

    create reusable dropdown comp for filtering

---
+++ src/components/dropdown/Dropdown.js
@@ -0,0 +1,50 @@
+import PropTypes from "prop-types";
+import styles from "./Dropdown.module.css";
+
+const Dropdown = ({
+  label,
+  value,
+  data,
+  customClass,
+  name,
+  onChange,
+  ...rest
+}) => {
+  return (
+    <div className={`${styles["form-group--select"]} ${customClass}`}>
+      <select
+        name={name}
+        value={value}
+        className="form-control"
+        onChange={onChange}
+        {...rest}
+      >
+        <option value="">{label}</option>
+        {data.map((item, key) => (
+          <option key={key} value={item}>
+            {item}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+Dropdown.propTypes = {
+  name: PropTypes.string,
+  value: PropTypes.string,
+  label: PropTypes.string,
+  placeholder: PropTypes.string,
+  data: PropTypes.array.isRequired,
+  customClass: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+};
+
+Dropdown.defaultProps = {
+  label: "",
+  value: "",
+  customClass: "",
+  placeholder: "",
+};
+
+export default Dropdown;
