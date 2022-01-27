829618a1df01744dd81aad5d25f5537341945c28
Aysel Cavdar <a.misrikhanova13@gmail.com>
2022-01-28 01:45:02 +0300

    implement validation for image size

--- src/components/create-app-form/CreateAppForm.js
+++ src/components/create-app-form/CreateAppForm.js
@@ -1,17 +1,15 @@
-import { Formik, Form } from 'formik';
+import { Formik, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import styles from './CreateAppForm.module.css';
 import FormInput from '../form-input/FormInput';
 import { useWindowSize } from '../../hooks/useWindowSize';
-import { useState } from 'react';
-import { imgHandler, removeImg } from '../../helper/imgUpload';
 import { useNavigate } from 'react-router-dom';
 import Button from '../button/Button';
+import { imgHandler } from '../../helper/imgUpload';
 import { useDispatch } from 'react-redux';
 import { createApplication } from '../../redux/actions/crudActions';
 
 const CreateAppForm = () => {
-  const [profileImg, setProfileImg] = useState(null);
   const [width] = useWindowSize();
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -25,8 +23,10 @@ const CreateAppForm = () => {
     reason: '',
     address: '',
     photo: '',
-    tc:'',
+    tc: '',
   };
+
+  const FILE_SIZE = 1e7;
 
   const validate = Yup.object({
     firstName: Yup.string()
@@ -45,31 +45,25 @@ const CreateAppForm = () => {
       .required('Zorunlu alan'),
     tcNum: Yup.string()
       .trim()
-      .matches(
-        tcRegx,
-        'Tc kimlik numarası geçerli değil.'
-      )
+      .matches(tcRegx, 'Tc kimlik numarası geçerli değil.')
       .required('Zorunlu alan'),
     reason: Yup.string()
       .trim()
-      .min(20, '20 karakter veya daha üzeri olmalı')
+      .min(10, '10 karakter veya daha üzeri olmalı')
       .max(255, '255 karater veya daha altı olmalı')
       .required('Zorunlu alan'),
     address: Yup.string()
       .trim()
-      .min(20, '20 karakter veya daha üzeri olmalı')
+      .min(10, '10 karakter veya daha üzeri olmalı')
       .max(255, '255 karater veya daha altı olmalı')
       .required('Zorunlu alan'),
     tc: Yup.string()
       .trim()
-      .matches(
-        tcRegx,
-        'Tc kimlik numarası geçerli değil.'
-      )
+      .matches(tcRegx, 'Tc kimlik numarası geçerli değil.'),
+    photo: Yup.mixed().required('Zorunlu alan'),
   });
 
   const handleSubmit = (values) => {
-    values['photo'] = profileImg;
     dispatch(createApplication(values));
     navigate(`/basvurularim/${values.tcNum}`);
   };
@@ -77,6 +71,7 @@ const CreateAppForm = () => {
   const showApplications = (values) => {
     tcRegx.test(values.tc) ? navigate(`/basvurularim/${values.tc}`) : false;
   };
+
   return (
     <div
       className={styles['application-container']}
@@ -84,91 +79,97 @@ const CreateAppForm = () => {
         justifyContent: width < 1024 && 'center',
       }}
     >
-      <>
-        <Formik
-          initialValues={initialValues}
-          validationSchema={validate}
-          onSubmit={(values, { resetForm }) => {
-            handleSubmit(values);
-            resetForm({});
-          }}
-        >
-          {({ values }) => (
-            <div>
-              <p className={styles['already-have']}>
-                Mevcut başvurularımı görüntülemek istiyorum.
-              </p>
-              <div className={styles['query-container']}>
-                <FormInput
-                  name="tc"
-                  placeholder="Tc kimlik numaranızı giriniz"
-                  type="text"
-                  label={''}
-                  customClass={styles['custom-input']}
-                />
-                <Button
-                  type="button"
-                  content={'Görüntüle'}
-                  onClick={() => showApplications(values)}
-                />
-              </div>
-              <hr />
-              <h1
-                style={{
-                  textAlign: width < 1024 && 'center',
-                  color: 'rgb(107, 99, 255)',
-                  marginTop: '0px',
-                }}
-              >
-                Basvuru Olustur
-              </h1>
-              <Form>
-                <FormInput label="Ad" name="firstName" type="text" />
-                <FormInput label="Soyad" name="lastName" type="text" />
-                <FormInput label="Yaş" name="age" type="number" />
-                <FormInput label="TC" name="tcNum" type="text" />
-                <FormInput
-                  label="Basvuru Nedeni"
-                  name="reason"
-                  type="textarea"
-                />
-                <FormInput label="Adress" name="address" type="textarea" />
-                <div className={styles['main-file--container']}>
-                  <div className={styles['file-input--container']}>
-                    <FormInput
-                      label="Fotograf Yükle"
-                      name="photo"
-                      type="file"
-                      accept="image/*"
-                      onChange={(e) => imgHandler(e, setProfileImg)}
-                      className={styles['file-input']}
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validate}
+        onSubmit={(values, { resetForm }) => {
+          handleSubmit(values);
+          resetForm({});
+        }}
+        validate={(values) => {
+          const errors={};
+          if(values?.photo){
+            const base64str = values?.photo?.split("base64,")[1];
+            const decoded = atob(base64str);
+            if (decoded?.length > FILE_SIZE) {
+              errors.photo ="Fotoğraf boyutunuz çok büyük 9MB altı yüklemeyi deneyin.";
+              return errors;
+            }
+          }
+        }}
+      >
+        {({ values, setFieldValue, errors, touched }) => (
+          <div>
+            <p className={styles['already-have']}>
+              Mevcut başvurularımı görüntülemek istiyorum.
+            </p>
+            <div className={styles['query-container']}>
+              <FormInput
+                name="tc"
+                placeholder="Tc kimlik numaranızı giriniz"
+                type="text"
+                label={''}
+                customClass={styles['custom-input']}
+              />
+              <Button
+                type="button"
+                content={'Görüntüle'}
+                onClick={() => showApplications(values)}
+              />
+            </div>
+            <hr />
+            <h1
+              style={{
+                textAlign: width < 1024 && 'center',
+                color: 'rgb(107, 99, 255)',
+                marginTop: '0px',
+              }}
+            >
+              Basvuru Olustur
+            </h1>
+            <Form>
+              <FormInput label="Ad" name="firstName" type="text" />
+              <FormInput label="Soyad" name="lastName" type="text" />
+              <FormInput label="Yaş" name="age" type="number" />
+              <FormInput label="TC" name="tcNum" type="text" />
+              <FormInput label="Basvuru Nedeni" name="reason" type="textarea" />
+              <FormInput label="Adress" name="address" type="textarea" />
+              <div className={styles['main-file--container']}>
+                <div className={styles['file-input--container']}>
+                  <label style={{ marginBottom: '1rem' }}>Fotoğraf</label>
+                  <input
+                    name="photo"
+                    type="file"
+                    accept="image/*"
+                    onChange={(e) => imgHandler(e, 'photo', setFieldValue)}
+                  />
+                  {errors.photo && touched.photo && (
+                    <ErrorMessage
+                      component="div"
+                      name={'photo'}
+                      className={styles.error}
                     />
-                  </div>
-                  {profileImg && (
-                    <div className={styles['uploaded-img--container']}>
-                      <div
-                        className={styles['remove-image']}
-                        onClick={() => removeImg(setProfileImg)}
-                      >
-                        &#x2715;
-                      </div>
-                      <img
-                        src={profileImg}
-                        alt="uploaded_image"
-                        width={'160px'}
-                        height={'160px'}
-                      />
-                    </div>
                   )}
                 </div>
-                <div className={styles['btn-container']}>
-                  <Button type="submit" content={'Gönder'} />
-                </div>
-              </Form>
-            </div>
-          )}
-        </Formik>
-      </>
+                {values?.photo && (
+                  <div className={styles['uploaded-img--container']}>
+                    <img
+                      src={values?.photo}
+                      alt="uploaded_image"
+                      width={'160px'}
+                      height={'160px'}
+                    />
+                  </div>
+                )}
+              </div>
+              <div className={styles['btn-container']}>
+                <Button type="submit" content={'Gönder'} />
+              </div>
+            </Form>
+          </div>
+        )}
+      </Formik>
+
       <div>
         <img
           src={require('../../assets/images/form1.png')}
--- src/helper/imgUpload.js
+++ src/helper/imgUpload.js
@@ -1,15 +1,12 @@
-
 // take 2 params; first one is event object, second one is setter of state func
-const imgHandler = (e, setter) => {
+const imgHandler = (e, field, setter) => {
   const reader = new FileReader();
   reader.onload = () => {
     if (reader.readyState === 2) {
-      setter(reader.result);
+      setter(field, reader.result);
     }
   };
   reader.readAsDataURL(e.target.files[0]);
 };
 
-const removeImg = (setter) => setter(null);
-
-export { imgHandler, removeImg };
+export { imgHandler };
