4c440b7a42e32d840182b4f1bf2c895f3d8ee993
Aysel Cavdar <a.misrikhanova13@gmail.com>
2022-01-28 14:34:23 +0300

    configure setupTest file

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,15 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import React from 'react';
+import { configure, shallow, render, mount } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+configure({ adapter: new Adapter() });
+
+// Make Enzyme functions available in all test files without importing
+global.React = React;
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
+
